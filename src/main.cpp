05df3dd149cb3be336faf14e25397bd84cade2f4
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-12 09:52:22 +0530

    Added code to ensure that HC-05 is in AT mode.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 void setup() {
 // write your initialization code here
+    Serial.begin(38400); //PC Serial Monitor
+    Serial1.begin(38400); //HC-05 AT Mode
 }
 
 void loop() {
 // write your code here
+
+    //PC --> Bluetooth
+    if (Serial.available()) {
+        Serial1.write(Serial.read());
+    }
+
+    //Bluetooth --> PC
+    if (Serial1.available()) {
+        Serial.write(Serial1.read());
+    }
 }
