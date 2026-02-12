fb3f38e0ce0b30fcc2c351c0dda843296538c6e9
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-12 14:02:44 +0530

    Added code to ensure that HC-05 is in normal mode.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include <Arduino.h>
 void setup() {
 // write your initialization code here
-    Serial.begin(38400); //PC Serial Monitor
-    Serial1.begin(38400); //HC-05 AT Mode
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
+    Serial1.begin(9600);
 }
-
 void loop() {
 // write your code here
-
-    //PC --> Bluetooth
-    if (Serial.available()) {
-        Serial1.write(Serial.read());
-    }
-
-    //Bluetooth --> PC
     if (Serial1.available()) {
-        Serial.write(Serial1.read());
+        char data= Serial1.read();
+        Serial.println(data);
+
+        if (data == '1') digitalWrite(13, HIGH);
+        if (data == '0') digitalWrite(13, LOW);
     }
 }
